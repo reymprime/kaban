a45b77553c81e2e929ae6a061091f684b98b7bc6
reymprime <reymprime@gmail.com>
2026-07-17 12:08:31 +0800

    update

--- src/components/CreditModal.svelte
+++ src/components/CreditModal.svelte
@@ -2,7 +2,7 @@
   import { lockScroll } from '../lib/scrollLock.js';
   let { onclose } = $props();
 
-  const version = 'v39';
+  const version = 'v40';
 
   function onBackdrop(e) {
     if (e.target === e.currentTarget) onclose();
